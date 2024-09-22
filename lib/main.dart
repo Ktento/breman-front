c2dba17a143ded4bfd2f8e18e6a8cde97d1eaa6a
大石翔亜 <toa.raple@gmail.com>
2024-09-22 23:24:19 +0900

    MaterialAp,Scaffoldを追加、’ホーム’を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,31 @@
 import 'package:flutter/material.dart';
 
-//メイン関数
+// メイン関数
 void main() {
-  final a = Column( //縦並びにする
-    mainAxisSize: MainAxisSize.min, // Columnのサイズを中身に合わせる
-    children: [
-      Container(  //コンテナ要素
-        color: Colors.black, //背景色
-        width: 1000, //幅
-        height: 100, //高さ
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Align(
+        alignment: Alignment.topCenter, // 上揃え
+        child: Column( // 縦並びにする
+          mainAxisSize: MainAxisSize.min, // Columnのサイズを中身に合わせる
+          children: [
+            Container(  // コンテナ要素
+              color: const Color.fromARGB(255, 156, 156, 156), // 背景色
+              width: 1000, // 幅
+              height: 100, // 高さ
+              child: Center(
+                child: Text('ホーム', style: TextStyle(fontSize: 20)), // Container内のテキスト
+              ),
+            ),
+            SizedBox(height: 20), // 2つのコンテナ間にスペースを追加
+            Container(
+              color: Colors.red, // 背景色
+              width: 1000, // 幅
+              height: 100, // 高さ
+            ),
+          ],
+        ),
       ),
-      SizedBox(height: 20), // 2つのコンテナ間にスペースを追加
-      Container(
-        color: Colors.red, //背景色
-        width: 1000, //幅
-        height: 100, //高さ
-      ),
-    ],
-  );
-
-  runApp(a);
+    ),
+  ));
 }
