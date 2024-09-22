bca5f15a6d82e3ee21687d769f2587d0162b0136
大石翔亜 <toa.raple@gmail.com>
2024-09-22 23:42:08 +0900

    アイコンボタンを追加し、右側に余白を入れた

--- lib/main.dart
+++ lib/main.dart
@@ -7,23 +7,44 @@ void main() {
       backgroundColor: Colors.white,
       body: Align(
         alignment: Alignment.topCenter, // 上揃え
-        child: Column( // 縦並びにする
+        child: Column(
+          // 縦並びにする
           mainAxisSize: MainAxisSize.min, // Columnのサイズを中身に合わせる
           children: [
-            Container(  // コンテナ要素
-            color: const Color.fromARGB(135, 182, 179, 179),
+            Container(
+              // コンテナ要素
+              color: const Color.fromARGB(135, 182, 179, 179),
               width: 1000, // 幅
               height: 80, // 高さ
-              child: Align(
-                alignment: Alignment.centerLeft, // 左揃えに設定
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 16.0), // 左に余白を追加
-                  child: Text('ホーム', 
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold, // 太字に設定
-                    )),
-                ),
+              child: Row(
+                // 横並びにする
+                children: [
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.centerLeft, // 左揃えに設定
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 16.0), // 左に余白を追加
+                        child: Text(
+                          'ホーム',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold, // 太字に設定
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 16.0), // 右に余白を追加
+                    child: IconButton(
+                      icon: Icon(Icons.account_circle, size: 30), // アカウントアイコン
+                      onPressed: () {
+                        // アイコンボタンが押された時の処理
+                        print('アカウントボタンが押されました');
+                      },
+                    ),
+                  ),
+                ],
               ),
             ),
             SizedBox(height: 20), // 2つのコンテナ間にスペースを追加
