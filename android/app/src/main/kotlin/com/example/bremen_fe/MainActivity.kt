565aff81b4d72c15915badef93c26fe62153afdf
大石翔亜 <toa.raple@gmail.com>
2024-09-22 23:04:43 +0900

    firstcomit

---
+++ android/app/src/main/kotlin/com/example/bremen_fe/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bremen_fe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+//メイン関数
+void main() {
+  final a = Align(
+    alignment: Alignment.topCenter, // 画面の上に配置
+    child: Container(
+      color: Colors.black, //コンテナの背景色を黒色に設定
+      width: 1000, //幅
+      height: 100, //高さ
+    ),
+  );
+
+  runApp(a);
+}
