74d137e47c2b15b51a6c38759b5ba31293027d74
大石翔亜 <toa.raple@gmail.com>
2024-09-25 00:03:49 +0900

    ログイン画面(アカウント作成からコピペ)

---
+++ lib/login.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // 戻るボタンを含むコンテナ
+            Container(
+              padding: EdgeInsets.only(top: 20, left: 16, right: 16), // 上部余白を調整
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.arrow_back, size: 30),
+                    onPressed: () {
+                      Navigator.pop(context); // 前の画面に戻る
+                    },
+                  ),
+                  SizedBox(width: 8), // 戻るボタンとテキストの間にスペースを追加
+                  Text(
+                    'プロフィール',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+
+            // その他のコンテンツをここに追加
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'ここにアカウント情報を表示',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
