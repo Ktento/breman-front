f7bc0767417ff94be6bc9ba327b51cb2958752b1
大石翔亜 <toa.raple@gmail.com>
2024-09-26 01:25:34 +0900

    みす

--- lib/account_profile.dart
+++ lib/account_profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'profile_edit.dart';
 
 class AccountProfile extends StatelessWidget {
   @override
@@ -28,13 +29,34 @@ class AccountProfile extends StatelessWidget {
                 ],
               ),
             ),
-            
+
             // その他のコンテンツをここに追加
             Container(
               padding: const EdgeInsets.all(16.0),
-              child: Text(
-                'ここにアカウント情報を表示',
-                style: TextStyle(fontSize: 18),
+              color: const Color.fromARGB(110, 118, 253, 88),
+              width: MediaQuery.of(context).size.width, // 画面の幅に合わせる
+              height: 100, // 高さ
+            ),
+            Container(
+              color: const Color.fromARGB(109, 40, 255, 90),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            ProfileEdit()), // ProfileEdit画面に遷移
+                  );
+                },
+                child: const Text('アカウント作成'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.orange,
+                  foregroundColor: Colors.white,
+                  minimumSize: Size(150, 30), // ボタンの最小サイズを指定
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(7), // 角の丸みを調整
+                  ),
+                ),
               ),
             ),
           ],
